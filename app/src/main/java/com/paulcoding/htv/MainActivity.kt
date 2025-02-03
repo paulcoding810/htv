83b196c04540b9258a5e5d7b59996c421ac574e8
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-02-03 10:36:47 +0700

    enable FLAG_SECURE

--- app/src/main/java/com/paulcoding/htv/MainActivity.kt
+++ app/src/main/java/com/paulcoding/htv/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.paulcoding.htv
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +11,12 @@ import com.paulcoding.htv.ui.theme.HTVTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+
         enableEdgeToEdge()
         setContent {
             HTVTheme {
